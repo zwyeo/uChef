d713ccf4303186cdc077437d2facb9f2e6534178
Hoseph132 <joseph.ho1996@gmail.com>
2022-11-11 20:48:11 +0800

    instruction list change

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,10 @@
   <!-- Bootstrap Navbar Start -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link to="/"
-        ><a class="nav-brand"
-        ><img src="../assets/logo.png" style="height: 10%; width:10%;" alt="uChef" /></a
-      ></router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <router-link to="/"><a class="nav-brand"><img src="../assets/logo.png" style="height: 10%; width:10%;"
+            alt="uChef" /></a></router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -18,13 +17,16 @@
             <a class="nav-link" href="#">Link</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
               Dropdown
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
@@ -59,44 +61,25 @@
       <div class="container">
         <!-- filter buttons -->
 
-        <select
-          class="form-select w-25 float-end"
-          aria-label="Default select example"
-          id="filter"
-          v-model="$store.state.selectedCategory"
-          @change="$store.dispatch('filterCategory')"
-        >
+        <select class="form-select w-25 float-end" aria-label="Default select example" id="filter"
+          v-model="$store.state.selectedCategory" @change="$store.dispatch('filterCategory')">
           <option v-for="cat of $store.state.foodCategories">{{ cat }}</option>
         </select>
         <label for="filter" class="float-end pe-2 pt-2">Filter By:</label>
-        <div
-          class="d-flex text-center rounded-pill w-50 pe-2 border border-secondary px-2 mt-2 bg-white"
-        >
+        <div class="d-flex text-center rounded-pill w-50 pe-2 border border-secondary px-2 mt-2 bg-white">
           <div class="pt-2 icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-search mb-1"
-              viewBox="0 0 16 16"
-              @click="$store.dispatch('getRecipes')"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search mb-1"
+              viewBox="0 0 16 16" @click="$store.dispatch('getRecipes')">
               <path
-                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-              ></path>
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z">
+              </path>
             </svg>
           </div>
           <router-link :to="{ name: 'home', hash: '#popular-recipe' }">
-            <input
-              type="text"
-              :placeholder="$store.state.searchDesc"
-              class="text-light w-5 form-control rounded-pill input border-0 bg-white"
-              v-model="searchQuery"
-              style="background-color: #40ba37"
-              @keyup.enter="onSearch"
-              @change="$store.dispatch('showPreviews'), closeSearchBox"
-            />
+            <input type="text" :placeholder="$store.state.searchDesc"
+              class="text-light w-5 form-control rounded-pill input border-0 bg-white" v-model="searchQuery"
+              style="background-color: #40ba37" @keyup.enter="onSearch"
+              @change="$store.dispatch('showPreviews'), closeSearchBox" />
           </router-link>
         </div>
       </div>
@@ -109,16 +92,11 @@
           <!-- Menu -->
           <nav class="classy-navbar justify-content-between" id="deliciousNav">
             <!-- Logo -->
-            <router-link to="/"
-              ><a class="nav-brand"
-                ><img src="../assets/logo.png" alt="uChef" /></a
-            ></router-link>
+            <router-link to="/"><a class="nav-brand"><img src="../assets/logo.png" alt="uChef" /></a></router-link>
 
             <!-- Navbar Toggler -->
             <div class="classy-navbar-toggler">
-              <span class="navbarToggler"
-                ><span></span><span></span><span></span
-              ></span>
+              <span class="navbarToggler"><span></span><span></span><span></span></span>
             </div>
 
             <!-- Menu -->
@@ -155,13 +133,9 @@
 
                 <!-- Profile -->
                 <div class="dropdown">
-                  <button
-                    class="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                    id="dropdownMenuButton1"
-                  ><img src="../assets/img/core-img/hamburger2.png" alt=""></button>
+                  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false" id="dropdownMenuButton1"><img src="../assets/img/core-img/hamburger2.png"
+                      alt=""></button>
                   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                     <li><a class="dropdown-item" href="#">Action</a></li>
                     <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -183,22 +157,13 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Modal title</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <p>Modal body text goes here.</p>
         </div>
         <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
             Close
           </button>
           <button type="button" class="btn btn-primary">Save changes</button>
@@ -258,7 +223,7 @@ export default {
       this.closeSearchBox();
     },
   },
-  created() {},
+  created() { },
 };
 </script>
 
@@ -274,6 +239,7 @@ a {
   width: 50px;
   height: 50px;
 }
+
 .navbar {
   position: sticky;
   z-index: 50;
@@ -297,6 +263,7 @@ a {
   box-shadow: none;
   text-decoration: none;
 }
+
 /* Start */
 .search-wrapper {
   width: 100%;
@@ -309,10 +276,12 @@ a {
   -webkit-transition-duration: 700ms;
   transition-duration: 700ms;
 }
+
 .search-wrapper form {
   position: relative;
   z-index: 1;
 }
+
 .search-wrapper form input {
   width: 90%;
   height: 40px;
@@ -322,11 +291,13 @@ a {
   padding: 0 20px;
   margin: 15px 0;
 }
+
 @media only screen and (max-width: 767px) {
   .search-wrapper form input {
     width: 80%;
   }
 }
+
 .search-wrapper form button {
   position: absolute;
   width: 60px;
@@ -338,11 +309,13 @@ a {
   cursor: pointer;
   outline: none;
 }
+
 @media only screen and (max-width: 767px) {
   .search-wrapper form button {
     right: 20%;
   }
 }
+
 .search-wrapper .close-btn {
   position: absolute;
   width: 70px;
@@ -358,6 +331,7 @@ a {
   top: 0;
   z-index: 100;
 }
+
 .search-wrapper.on {
   top: 0;
 }
@@ -367,12 +341,14 @@ a {
   z-index: 100;
   width: 100%;
 }
+
 .header-area .top-header-area {
   width: 100%;
   height: 50px;
   background-color: #ffffff;
   border-bottom: 1px solid #dddee9;
 }
+
 @media only screen and (max-width: 767px) {
   .header-area .top-header-area {
     height: 70px;
@@ -385,31 +361,38 @@ a {
   z-index: 100;
   height: 145px;
 }
+
 @media only screen and (max-width: 767px) {
   .header-area .delicious-main-menu {
     height: 80px;
   }
 }
+
 .header-area .delicious-main-menu .classy-nav-container {
   background-color: transparent;
 }
+
 .header-area .delicious-main-menu .classy-navbar {
   height: 145px;
   padding: 0;
 }
+
 @media only screen and (max-width: 767px) {
   .header-area .delicious-main-menu .classy-navbar {
     height: 80px;
   }
 }
+
 .header-area .delicious-main-menu .classy-navbar .classynav ul li a {
   font-weight: 600;
 }
+
 .header-area .delicious-main-menu .classy-navbar .classynav ul li a:hover,
 .header-area .delicious-main-menu .classy-navbar .classynav ul li a:focus {
   font-weight: 600;
 }
-.header-area .delicious-main-menu .classy-navbar .classynav > ul > li > a {
+
+.header-area .delicious-main-menu .classy-navbar .classynav>ul>li>a {
   text-transform: uppercase;
   padding: 12px 18px 11px;
   background-color: transparent;
@@ -418,55 +401,30 @@ a {
   color: #474747;
   font-weight: 600;
 }
-.header-area .delicious-main-menu .classy-navbar .classynav > ul > li > a:hover,
-.header-area
-  .delicious-main-menu
-  .classy-navbar
-  .classynav
-  > ul
-  > li
-  > a:focus {
+
+.header-area .delicious-main-menu .classy-navbar .classynav>ul>li>a:hover,
+.header-area .delicious-main-menu .classy-navbar .classynav>ul>li>a:focus {
   font-size: 14px;
   color: #ffffff;
   background-color: #40ba37;
   border-bottom: 3px solid #1c8314;
 }
-.header-area
-  .delicious-main-menu
-  .classy-navbar
-  .classynav
-  > ul
-  > li
-  > a:hover::after,
-.header-area
-  .delicious-main-menu
-  .classy-navbar
-  .classynav
-  > ul
-  > li
-  > a:focus::after {
+
+.header-area .delicious-main-menu .classy-navbar .classynav>ul>li>a:hover::after,
+.header-area .delicious-main-menu .classy-navbar .classynav>ul>li>a:focus::after {
   color: #ffffff;
 }
-.header-area
-  .delicious-main-menu
-  .classy-navbar
-  .classynav
-  > ul
-  > li.active
-  > a {
+
+.header-area .delicious-main-menu .classy-navbar .classynav>ul>li.active>a {
   color: #ffffff;
   background-color: #40ba37;
   border-bottom: 3px solid #1c8314;
 }
-.header-area
-  .delicious-main-menu
-  .classy-navbar
-  .classynav
-  > ul
-  > li.active
-  > a::after {
+
+.header-area .delicious-main-menu .classy-navbar .classynav>ul>li.active>a::after {
   color: #ffffff;
 }
+
 .header-area .delicious-main-menu .classynav .search-btn {
   color: #b6b6b6;
   margin-left: 150px;
@@ -474,37 +432,44 @@ a {
   -webkit-transition-duration: 500ms;
   transition-duration: 500ms;
 }
+
 .header-area .delicious-main-menu .classynav .search-btn i {
   -webkit-transition-duration: 500ms;
   transition-duration: 500ms;
 }
+
 .header-area .delicious-main-menu .classynav .search-btn:hover i,
 .header-area .delicious-main-menu .classynav .search-btn:focus i {
   color: #40ba37;
 }
+
 @media only screen and (min-width: 992px) and (max-width: 1199px) {
   .header-area .delicious-main-menu .classynav .search-btn {
     margin-left: 50px;
     margin-bottom: 20px;
   }
 }
+
 @media only screen and (min-width: 768px) and (max-width: 991px) {
   .header-area .delicious-main-menu .classynav .search-btn {
     margin-left: 30px;
     margin-top: px;
   }
 }
+
 @media only screen and (max-width: 767px) {
   .header-area .delicious-main-menu .classynav .search-btn {
     margin-left: 30px;
     margin-top: 30px;
   }
 }
+
 @media only screen and (max-width: 767px) {
   .header-area .nav-brand img {
     max-width: 90px;
   }
 }
+
 .nav-brand {
   width: 90px;
 }
