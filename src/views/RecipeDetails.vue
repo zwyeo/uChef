b29d3afcebb1e47ac0447d4e5dfc52b292139c6a
jadengohzh <jadengoh1999@gmail.com>
2022-11-12 19:13:48 +0800

    fix reviews section

--- src/views/RecipeDetails.vue
+++ src/views/RecipeDetails.vue
@@ -23,27 +23,48 @@
             <!-- IF VIDEO EXISTS -->
             <div v-if="videoExist == true">
               <!-- video button -->
-              <button class="btn video-btn" data-bs-toggle="modal" :data-src="video" data-bs-target="#myModal"
-                @click="open()">
+              <button
+                class="btn video-btn"
+                data-bs-toggle="modal"
+                :data-src="video"
+                data-bs-target="#myModal"
+                @click="open()"
+              >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                   <path
-                    d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
+                    d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+                  />
                 </svg>
                 Watch Video
               </button>
 
               <!-- Modal -->
-              <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
+              <div
+                class="modal fade"
+                id="myModal"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
                 <div class="modal-dialog" role="document">
                   <div class="modal-content">
                     <div class="modal-body">
-                      <button type="button" class="btn-close" @click="close()" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        @click="close()"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
                       <!-- 16:9 aspect ratio -->
                       <div class="ratio ratio-16x9">
-                        <iframe class="embed-responsive-item" :src="video2" allowscriptaccess="always"
-                          allow="autoplay"></iframe>
+                        <iframe
+                          class="embed-responsive-item"
+                          :src="video2"
+                          allowscriptaccess="always"
+                          allow="autoplay"
+                        ></iframe>
                       </div>
                     </div>
                   </div>
@@ -51,7 +72,6 @@
               </div>
             </div>
             <!-- END of v-if-->
-
           </div>
         </div>
 
@@ -105,13 +125,26 @@
                 <i class="fa fa-star" aria-hidden="true"></i>
                 <i class="fa fa-star" aria-hidden="true"></i>
               </div>
-
             </div>
 
-            <a v-if="!bookmarked" href="#" class="btn delicious-btn" @click="bookmark()" data-bs-toggle="modal"
-              data-bs-target="#bookmarkModal">Bookmark</a>
-            <a v-else href="#" class="btn delicious-btn" @click="unbookmark()" data-bs-toggle="modal"
-              data-bs-target="#unbookmarkModal">Bookmarked!</a>
+            <a
+              v-if="!bookmarked"
+              href="#"
+              class="btn delicious-btn"
+              @click="bookmark()"
+              data-bs-toggle="modal"
+              data-bs-target="#bookmarkModal"
+              >Bookmark</a
+            >
+            <a
+              v-else
+              href="#"
+              class="btn delicious-btn"
+              @click="unbookmark()"
+              data-bs-toggle="modal"
+              data-bs-target="#unbookmarkModal"
+              >Bookmarked!</a
+            >
           </div>
         </div>
       </div>
@@ -119,7 +152,11 @@
       <div class="row">
         <div class="col-12 col-lg-8">
           <!-- Single Preparation Step -->
-          <div v-for="(item, index) in instructions" :key="item" class="single-preparation-step d-flex">
+          <div
+            v-for="(item, index) in instructions"
+            :key="item"
+            class="single-preparation-step d-flex"
+          >
             <h4 v-if="index < 9">0{{ index + 1 }}</h4>
             <h4 v-else>{{ index + 1 }}</h4>
             <p>{{ item }}</p>
@@ -131,14 +168,22 @@
           <h4 class="">Ingredients</h4>
 
           <!-- Ingredient list -->
-          <div v-for="(item, index) in ingredient_list" :key="index" :id="index">
+          <div
+            v-for="(item, index) in ingredient_list"
+            :key="index"
+            :id="index"
+          >
             <ul class="list-group text-white">
-              <li class="list-group-item d-flex justify-content-between align-content-center">
+              <li
+                class="list-group-item d-flex justify-content-between align-content-center"
+              >
                 <h6 class="mb-0">
-                  <img src="../assets/img/core-img/salad.png" class="ingredient-img" />
+                  <img
+                    src="../assets/img/core-img/salad.png"
+                    class="ingredient-img"
+                  />
                   &nbsp;<span class="item">{{ item }}</span>
                 </h6>
-
               </li>
             </ul>
 
@@ -153,9 +198,17 @@
             <h3>Reviews</h3>
             <p>See what others have to say</p>
           </div>
-          <div v-if="review_list.length > 0" id="reviewsSection">
-            <review-card v-for="(review, index) of review_list" :key="index" :id="id" :user="review.user"
-              :date="review.date" :rating="review.rating" :subject="review.subject" :message="review.message">
+          <div v-if="review_list.length > 0">
+            <review-card
+              v-for="(review, index) of review_list"
+              :key="index"
+              :id="id"
+              :user="review.user"
+              :date="review.date"
+              :rating="review.rating"
+              :subject="review.subject"
+              :message="review.message"
+            >
             </review-card>
           </div>
           <div v-else>
@@ -170,20 +223,35 @@
   <!-- ##### Follow Us Instagram Area End ##### -->
 
   <!-- Bookmark Modal -->
-  <div class="modal fade" id="bookmarkModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+  <div
+    class="modal fade"
+    id="bookmarkModal"
+    tabindex="-1"
+    aria-labelledby="ModalLabel"
+    aria-hidden="true"
+  >
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5 mx-auto" id="exampleModalLabel">
             Item bookmarked! 🥳
           </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body m-3 mx-auto">
           Awesome! {{ title }} will be in your bookmark page!
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-success mx-auto" data-bs-dismiss="modal">
+          <button
+            type="button"
+            class="btn btn-success mx-auto"
+            data-bs-dismiss="modal"
+          >
             Ok
           </button>
         </div>
@@ -192,20 +260,35 @@
   </div>
 
   <!-- Unbookmark Modal -->
-  <div class="modal fade" id="unbookmarkModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+  <div
+    class="modal fade"
+    id="unbookmarkModal"
+    tabindex="-1"
+    aria-labelledby="ModalLabel"
+    aria-hidden="true"
+  >
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5 mx-auto" id="exampleModalLabel">
             Awww 😥... Item unbookmarked!
           </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body m-3 mx-auto">
           {{ title }} will be removed your bookmark page!
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-success mx-auto" data-bs-dismiss="modal">
+          <button
+            type="button"
+            class="btn btn-success mx-auto"
+            data-bs-dismiss="modal"
+          >
             Ok
           </button>
         </div>
@@ -226,9 +309,9 @@ import {
   remove,
   update,
   updates,
-  set
+  set,
 } from "firebase/database";
-import { ref as sRef } from 'firebase/storage';
+import { ref as sRef } from "firebase/storage";
 import ReviewBtn from "../components/ReviewButton.vue";
 import ReviewCard from "../components/ReviewCard.vue";
 export default {
@@ -266,7 +349,7 @@ export default {
           for (let entry in response.data) {
             // console.log(response.data[entry].id)
             if (response.data[entry].id == this.id) {
-              var obj = response.data[entry]
+              var obj = response.data[entry];
               this.videoExist = false;
               this.title = obj.title;
               this.image = obj.image;
@@ -291,11 +374,10 @@ export default {
                 }
               }
             }
-
           }
-        })
+        });
     }
-    // 
+    //
     else {
       axios
         .get(url, {
@@ -338,27 +420,25 @@ export default {
       )
       .then((response) => {
         if (response.data != null) {
-          console.log(response.data)
-          var total_rating = 0
-          var num_of_rating = 0
+          console.log(response.data);
+          var total_rating = 0;
+          var num_of_rating = 0;
           for (let review in Object.values(response.data)[0]) {
             // console.log(Object.values(response.data)[0][review].rating)
-            total_rating += Number(Object.values(response.data)[0][review].rating);
+            total_rating += Number(
+              Object.values(response.data)[0][review].rating
+            );
             num_of_rating++;
           }
           var rating = total_rating / num_of_rating;
           var remainder = rating % 1;
           if (remainder == 0) {
-            this.rating = rating
+            this.rating = rating;
+          } else if (remainder >= 0.5) {
+            this.rating = Math.ceil(rating);
+          } else {
+            this.rating = Math.floor(rating);
           }
-          else if (remainder >= 0.5) {
-
-            this.rating = Math.ceil(rating)
-          }
-          else {
-            this.rating = Math.floor(rating)
-          }
-
         }
       })
       .catch((error) => {
@@ -383,19 +463,18 @@ export default {
         console.log(error);
       });
     //review card population
-    let rurl = `https://wad-proj-22042-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json`;
+    let rurl = `https://wad-proj-22042-default-rtdb.asia-southeast1.firebasedatabase.app/recipes/${this.id}/reviews.json`;
     axios.get(rurl).then((response) => {
       // console.log(response.data);
       let reviewsObj = response.data;
       for (let review in reviewsObj) {
         this.review_list.push(reviewsObj[review]);
       }
-      // console.log(this.review_list);
+
       this.review_list = this.review_list.reverse();
     });
   },
   methods: {
-
     // bookmarking functions
     bookmark() {
       console.log(this.$store.state.userId);
@@ -413,21 +492,28 @@ export default {
       //get current instances of the catogory in user's preferences
       var current = null;
       var str_id = null;
-      axios.get(`https://wad-proj-22042-default-rtdb.asia-southeast1.firebasedatabase.app/users/${this.$store.state.userId}/preferences.json`)
+      axios
+        .get(
+          `https://wad-proj-22042-default-rtdb.asia-southeast1.firebasedatabase.app/users/${this.$store.state.userId}/preferences.json`
+        )
         .then((response) => {
-          console.log(response.data)
+          console.log(response.data);
           for (let bookmark in response.data) {
             str_id = bookmark;
             current = response.data[bookmark][this.foodCategory];
-            console.log(current)
+            console.log(current);
           }
           var final = current + 1;
 
           const db = getDatabase();
-          set(ref(db, `users/${this.$store.state.userId}/preferences/${str_id}/${this.foodCategory}`), final);
-        }
-        )
-
+          set(
+            ref(
+              db,
+              `users/${this.$store.state.userId}/preferences/${str_id}/${this.foodCategory}`
+            ),
+            final
+          );
+        });
     },
     unbookmark() {
       const db = getDatabase();
@@ -441,20 +527,28 @@ export default {
       // reduce food category by 1 from user's preferences
       var current = null;
       var str_id = null;
-      axios.get(`https://wad-proj-22042-default-rtdb.asia-southeast1.firebasedatabase.app/users/${this.$store.state.userId}/preferences.json`)
+      axios
+        .get(
+          `https://wad-proj-22042-default-rtdb.asia-southeast1.firebasedatabase.app/users/${this.$store.state.userId}/preferences.json`
+        )
         .then((response) => {
-          console.log(response.data)
+          console.log(response.data);
           for (let bookmark in response.data) {
             str_id = bookmark;
             current = response.data[bookmark][this.foodCategory];
-            console.log(current)
+            console.log(current);
           }
           var final = current - 1;
 
           const db = getDatabase();
-          set(ref(db, `users/${this.$store.state.userId}/preferences/${str_id}/${this.foodCategory}`), final);
-        }
-        )
+          set(
+            ref(
+              db,
+              `users/${this.$store.state.userId}/preferences/${str_id}/${this.foodCategory}`
+            ),
+            final
+          );
+        });
     },
     //Video Modal Functions
     open() {
@@ -683,7 +777,6 @@ ol li {
 }
 
 @media only screen and (max-width: 767px) {
-
   .delicious-btn.btn-4.active,
   .delicious-btn.btn-4:hover,
   .delicious-btn.btn-4:focus {
@@ -854,14 +947,15 @@ input[type="checkbox"]:checked:after {
   left: 6px;
 }
 
-input[type="checkbox"]:checked+.check {
+input[type="checkbox"]:checked + .check {
   opacity: 1;
 }
 
 /*food image  */
 .fancy-border {
   border: 25px solid #b88846;
-  border-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='75' height='75'%3E%3Cg fill='none' stroke='%23B88846' stroke-width='2'%3E%3Cpath d='M1 1h73v73H1z'/%3E%3Cpath d='M8 8h59v59H8z'/%3E%3Cpath d='M8 8h16v16H8zM51 8h16v16H51zM51 51h16v16H51zM8 51h16v16H8z'/%3E%3C/g%3E%3Cg fill='%23B88846'%3E%3Ccircle cx='16' cy='16' r='2'/%3E%3Ccircle cx='59' cy='16' r='2'/%3E%3Ccircle cx='59' cy='59' r='2'/%3E%3Ccircle cx='16' cy='59' r='2'/%3E%3C/g%3E%3C/svg%3E") 25;
+  border-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='75' height='75'%3E%3Cg fill='none' stroke='%23B88846' stroke-width='2'%3E%3Cpath d='M1 1h73v73H1z'/%3E%3Cpath d='M8 8h59v59H8z'/%3E%3Cpath d='M8 8h16v16H8zM51 8h16v16H51zM51 51h16v16H51zM8 51h16v16H8z'/%3E%3C/g%3E%3Cg fill='%23B88846'%3E%3Ccircle cx='16' cy='16' r='2'/%3E%3Ccircle cx='59' cy='16' r='2'/%3E%3Ccircle cx='59' cy='59' r='2'/%3E%3Ccircle cx='16' cy='59' r='2'/%3E%3C/g%3E%3C/svg%3E")
+    25;
 }
 
 .display-img {
