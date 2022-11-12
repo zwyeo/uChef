0bed288e7d4a6d52c327bbc50b6a46e85d246f4f
UnseenBanana <66821601+UnseenBanana@users.noreply.github.com>
2022-11-12 13:40:54 +0800

    AddNewRecipe, RemoveRecipe, MyRecipes, UpdateRecipe

--- src/components/RemoveRecipe.vue
+++ src/components/RemoveRecipe.vue
@@ -5,16 +5,58 @@
 </template>
 
 <script>
-    import { getDatabase, ref, remove } from "firebase/database";
+    import { getDatabase, ref, get, remove } from "firebase/database";
 
     export default {
         name: "RemoveRecipe",
         data() {
             return {}
         },
+        props: ["recipe-id"],
         methods: {
             removeRecipe() {
-                console.log("remove recipe")
+                let userId = this.$store.state.userId;
+                // console.log("remove recipe");
+                // console.log(this.recipeId);
+
+                const db = getDatabase();
+                const recipesRef = ref(db, `community`);
+                const usersRef = ref(db, `users/${userId}/recipes`);
+                // console.log(recipesRef);
+                // console.log(usersRef);
+
+                get(recipesRef)
+                .then((snapshot) => {
+                    let data = snapshot.val();
+                    // console.log(data);
+                    for (let i in data) {
+                        // console.log(data[i]);
+                        if (data[i].id == this.recipeId) {
+                            let deleteRef = ref(db, `community/${i}`);
+                            remove(deleteRef).then(() => {
+                                console.log("Location removed");
+                            })
+                        }
+                    }
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                })
+
+                get(usersRef)
+                .then((snapshot) => {
+                    let data = snapshot.val();
+                    for (let i in data) {
+                        // console.log(data[i]);
+                        if (data[i].id == this.recipeId) {
+                            let deleteRef = ref(db, `users/${userId}/recipes/${i}`);
+                            remove(deleteRef).then(() => {
+                                console.log("Location removed");
+                            })
+                        }
+                    }
+                })
+                
             }
         }
     }
