32f3e7f8c279dcb47c98fc7821bb9c75169c8531
UnseenBanana <66821601+UnseenBanana@users.noreply.github.com>
2022-11-13 10:42:36 +0800

    Update RemoveRecipe.vue

--- src/components/RemoveRecipe.vue
+++ src/components/RemoveRecipe.vue
@@ -1,59 +1,10 @@
 <template>
-  <div>
     <button
       class="btn btn-danger"
-      data-bs-toggle="modal"
-      data-bs-target="#deleteModal"
+      @click="warning"
     >
       Delete
     </button>
-  </div>
-
-  <div
-    class="modal fade"
-    id="deleteModal"
-    tabindex="-1"
-    aria-labelledby="ModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">
-            Are you sure you want to delete?
-          </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          Deletion of recipe cannot be undone! Please think carefully! Press
-          comfirm to delete.
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-primary"
-            @click="removeRecipe"
-            data-bs-dismiss="modal"
-          >
-            Confirm
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
